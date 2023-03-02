8b302d979de2a788131a0385a375b3d6b484b71a
DevDecor <fiyiziyech@gmail.com>
2023-03-02 10:49:03 -0500

    Binary Tree node left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,11 +7,12 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *node = binary_tree_node(NULL, value), *p = NULL;
+	binary_tree_t *node, *p = NULL;
 
-	if (node == NULL)
-		return (NULL);
 	if (parent == NULL)
+		return (NULL);
+	node = binary_tree_node(NULL, value);
+	if (node == NULL)
 		return (NULL);
 	node->parent = parent;
 	p = parent->left;
